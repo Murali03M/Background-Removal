da0c14f2e5f0666bbb40ac8c1181d88e1315fd79
Murali Murugan <murali732000@gmail.com>
2024-10-29 00:37:48 +0000

    addd

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -13,21 +13,42 @@ const Result = () => {
 
     const { resultImage, image } = context;
 
+    const { removeBackground } = context;
+
+    const updateHander = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        const files = e.target.files;
+
+        if (!files || files.length===0 )
+        {
+            return
+        };
+
+        removeBackground(files[0]);
+
+
+
+
+
+        
+
+    }
+
     
   return (
-      <div className='mx-4 my-4 lgh:mx-44 mt-14 min-h-[75vh] '>
+      <div className='mx-4 my-4 lgh:mx-44 mt-14 min-h-[85vh] '>
           <div className='bg-white rounded-lg px-8 py-6 drop-shadow-sm '>
-              <div className='flex flex-col sm:grid grid-cols-2 gap-8'>
+              <div className='flex flex-col sm:grid grid-cols-2 gap-8 '>
                   <div>
                       <p className='font-semibold text-gray-600 mb-2'>Original</p>
-                      <img src={image? URL.createObjectURL(image):assets.image_w_bg} alt="image_w_bg" className='rounded-md border'/>
+                      <img src={image? URL.createObjectURL(image):assets.image_w_bg} alt="image_w_bg" className='rounded-md border bg-cover'/>
                   </div>
                   <div className='flex flex-col'>
                       <p className='font-semibold text-gray-600 mb-2' >BackGround Removed</p>
                       <div  className='rounded-md border border-gray-300 h-full relative bg-layer overflow-hidden'>
                           
                    
-                          <img src={resultImage ? resultImage : assets.image_wo_bg} alt="image_wo_bg" />
+                          <img src={resultImage ? resultImage : ""} alt="image_wo_bg"  className='bg-cover'/>
                           {
                               !resultImage && image &&   <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2l'>
                               <div className='border-4 border-violet-600 rounded-full h-12 w-12 animate-spin border-t-transparent'></div>
@@ -38,8 +59,9 @@ const Result = () => {
                   </div>
               </div> 
               {
-                  resultImage &&      <div className='flex sm:justify-end   justify-center items-center flex-wrap gap-6 mt-6'>
-                  <button className='px-8 py-2.5 text-violet-600 border border-violet-600 rounded-full hover:scale-105  transition-all duration-700 '>Try another image</button>
+                  resultImage && <div className='flex sm:justify-end   justify-center items-center flex-wrap gap-6 mt-6'>
+                      <input onChange={updateHander} accept="image/*" type="file" name="" id="upload3" hidden/>
+                  {/* <button className='px-8 py-2.5 text-violet-600 border border-violet-600 rounded-full hover:scale-105  transition-all duration-700 ' >Try another image</button> */}
                   <a href={resultImage|| ""} download className='px-8 py-2.5 bg-gradient-to-r from-violet-600 to-fuchsia-500 rounded-full hover:scale-105  transition-all duration-700 text-white'>Download image</a>
               </div>
               }
